42c74a5eb24c8316263c292017e984d8ba7263d9
“E1cheng” <yvonne1231cheng@gmail.com>
2024-04-19 02:17:04 +0000

    model done

--- scripts/03-model.R
+++ scripts/03-model.R
@@ -22,13 +22,13 @@ dataq <- read_parquet(here::here("data/analysis_data/agriculture.parquet"))
 
 
 model <- stan_glm(
-         climate_impacts_per_decade_percent ~ local_delta_t + annual_precipitation_change_each_study_mm + co2_ppm, 
-         data = dataq, 
-         family = gaussian,
-         prior = normal(location = 0, scale = 3, autoscale = TRUE),
-         
-         prior_intercept = normal(location = 0, scale = 3, autoscale = TRUE),
-         seed = 853)
+  climate_impacts_per_decade_percent ~ local_delta_t + annual_precipitation_change_each_study_mm + co2_ppm, 
+  data = dataq,
+  family = gaussian(),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_aux = exponential(rate = 1, autoscale = TRUE),
+  seed = 853)
 
 saveRDS(
   model,
